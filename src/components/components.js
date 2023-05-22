91901bcd9d1cb12a7805e94bac2f734449d032de
Mario Acosta <macosta2015@my.fit.edu>
2023-05-22 15:08:18 -0400

    Adding the first component.

---
+++ src/components/components.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IssueData = () => {
+
+    return(
+        <div>
+            <p>
+                We are in the IssueData
+            </p>
+        </div>
+    )
+};
+
+export default IssueData
