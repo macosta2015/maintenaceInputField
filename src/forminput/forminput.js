1ba81975d039dff6c1541ec2ab5f5f1f9d6d6370
Mario Acosta <macosta2015@my.fit.edu>
2023-05-23 11:07:50 -0400

    Making the input fields work

--- src/forminput/forminput.js
+++ src/forminput/forminput.js
@@ -1,14 +1,31 @@
 import {React, useState} from 'react';
 
 
-const IssueData = () => {
-    const [inputForm, setInputForm] = useState('')
 
+const IssueData = () => {
+    const [inputForm, setInputForm] = useState([])
+    const [formData, setformFData] = useState({name: "", email: "", message: ""});
+
+
+
+    function onChangeFunction(e){
+        console.log('Hello World');
+        setInputForm(e.target.value)
+    }
 
     return(
         <div>
             <p>
                 We are in the IssueData
+            </p>
+            <input
+            type="text"
+            value={inputForm}
+            onChange={onChangeFunction}
+            >
+            </input>
+            <p>
+            The value is {inputForm} !
             </p>
         </div>
     )
