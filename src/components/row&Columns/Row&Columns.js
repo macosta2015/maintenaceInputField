0c6991859659bf867b095932875523d8ebe98d2d
Mario Acosta <macosta2015@my.fit.edu>
2023-05-23 18:12:48 -0400

    Fixing the padd left and right

--- src/components/row&Columns/Row&Columns.js
+++ src/components/row&Columns/Row&Columns.js
@@ -22,9 +22,7 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function RowAndColumnSpacing() {
     return (
         <div className="grid-container">
-            <Grid class="father-grid">  
-              {/* Rowspacing is the key to space the children 'Grid' */}
-              <Grid container rowSpacing={'15%'} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+              <Grid container className="father-grid"rowSpacing={'15%'} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                 <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                     <div className="my-grid-item">
                         <Item>
@@ -48,8 +46,7 @@ export default function RowAndColumnSpacing() {
                   </Item>
                 </Grid>
               </Grid>        
-            </Grid>
-          </div>
+        </div>
       
         );
       }
