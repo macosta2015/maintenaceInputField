f15318153195a1733536de463e4d33cdf64819a0
macosta2015 <macosta2015@my.fit.edu>
2023-05-25 23:40:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <RowAndColumnSpacing />
-      <IssueData />
+      {/* <IssueData /> */}
       {/* <ResPerson/> */}
     </div>
   );
