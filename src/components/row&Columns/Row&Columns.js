ea78b04aa04f1154fe02061323e87f33dca7bcca
macosta2015 <macosta2015@my.fit.edu>
2023-05-25 23:44:29 -0400

    commit

--- src/components/row&Columns/Row&Columns.js
+++ src/components/row&Columns/Row&Columns.js
@@ -13,7 +13,6 @@ import ResPerson from '../resPerson/resPerson.js';
 import AreaInfo from '../areaInfo/areaInfo.js'
 import FileUpload from '../fileUpload/fileUpload.js'
 
-
 const message = `Fill up the information asked: `;
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -25,35 +24,35 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function RowAndColumnSpacing() {
-    return (
-        <div className="grid-container">
-              <Grid container className="father-grid"rowSpacing={'0%'} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                    <div className="my-grid-item">
-                        <Item>
-                            <Typography variant='h3'>
-                                {message}
-                            </Typography>
-                        </Item>
-                    </div>
-                </Grid>
-                <Grid item xs={12} sm={12} md={12} lg={6} xl={12} >
-                  <Item>
-                    <AreaInfo/>
-                  </Item>
-                </Grid>
-                <Grid item xs={12} sm={12} md={12} lg={6} xl={12} >
-                  <Item>
-                  <FileUpload/>
-                  </Item>
-                </Grid>
-                <Grid item xs={12} sm={12} md={12} lg={6} xl={12} sx={{ display: { xs: 'none', md: 'block' } }} >
-                  <Item>
-                    <IssueData/>                  
-                  </Item>
-                </Grid>
-              </Grid>        
-        </div>
-      
-        );
-      }
+  return (
+    <div className="grid-container">
+      <Grid container className="father-grid" rowSpacing={'0%'} columnSpacing={{ xs: 0, sm: 0, md: 0 }}>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <div className="my-grid-item">
+            <Item>
+              <Typography variant='h3'>
+                {message}
+              </Typography>
+            </Item>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={12} >
+          <Item>
+            <AreaInfo />
+          </Item>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={12} >
+          <Item>
+            <FileUpload />
+          </Item>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={12} sx={{ display: { xs: 'none', md: 'block' } }} >
+          <Item>
+            <IssueData />
+          </Item>
+        </Grid>
+      </Grid>
+    </div>
+
+  );
+}
