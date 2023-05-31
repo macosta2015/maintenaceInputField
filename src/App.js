1f19b1be260891ea5b15559d9c4d8076b990d9ec
macosta2015 <macosta2015@my.fit.edu>
2023-05-31 00:12:30 -0400

    Moving JSON packages around

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 // Import components
@@ -5,12 +6,17 @@ import IssueData from './components/formInput/formInput.js';
 import ResPerson from './components/resPerson/resPerson.js';
 import RowAndColumnSpacing from './components/row&Columns/Row&Columns.js';
 import FileUpload from './components/fileUpload/fileUpload.js'
+import MyForm from './components/useForm/useForm.js';
+
 
 
 function App() {
   return (
     <div className="App">
-      <RowAndColumnSpacing />
+
+      {/* <RowAndColumnSpacing /> */}
+      <MyForm />
+
       {/* <IssueData /> */}
       {/* <ResPerson/> */}
     </div>
