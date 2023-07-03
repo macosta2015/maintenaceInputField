1a9d5e3081c30be100cc0568caf37018404007db
Mario Acosta <macosta2015@my.fit.edu>
2023-07-03 14:32:13 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 // Import components
-import IssueData from './components/formInput/formInput.js';
+import IssueData from './components/formInput/formInput';
 import ResPerson from './components/resPerson/resPerson.js';
 import RowAndColumnSpacing from './components/row&Columns/Row&Columns.js';
 import FileUpload from './components/fileUpload/fileUpload.js'
-import MyForm from './components/useForm/useForm.js';
+import MyForm from './components/useForm/useForm';
 
 
 
